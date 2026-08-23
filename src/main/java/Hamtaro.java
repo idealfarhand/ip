e715644a8fd9bdd5a817eaf0d0f0ce64644dc477
Aidil <317012577+idealfarhand@users.noreply.github.com>
2026-08-23 10:15:49 +0800

    Change ChatBot name, Add greeting

---
+++ src/main/java/Hamtaro.java
@@ -0,0 +1,17 @@
+public class Hamtaro {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        String banner = "    __  __                __                 \n"
+                + "   / / / /___ _____ ___  / /_____ __________ \n"
+                + "  / /_/ / __ `/ __ `__ \\/ __/ __ `/ ___/ __ \\\n"
+                + " / __  / /_/ / / / / / / /_/ /_/ / /  / /_/ /\n"
+                + "/_/ /_/\\__,_/_/ /_/ /_/\\__/\\__,_/_/   \\____/ \n";
+        System.out.println(banner);
+        System.out.println("Hello! I'm Hamtaro!.");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+
+    }
+}
