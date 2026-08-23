3684738e84cec2e531c535d452094f7ff9a45682
Aidil <317012577+idealfarhand@users.noreply.github.com>
2026-08-23 10:59:18 +0800

    Add echo functionality

--- src/main/java/Hamtaro.java
+++ src/main/java/Hamtaro.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Hamtaro {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean flag = true;
         System.out.println("____________________________________________________________");
         String banner = "    __  __                __                 \n"
                 + "   / / / /___ _____ ___  / /_____ __________ \n"
@@ -10,8 +14,20 @@ public class Hamtaro {
         System.out.println("Hello! I'm Hamtaro!.");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
 
+        while(flag){
+            String userInput = scanner.nextLine();
+            if(userInput.equals("bye")){
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                flag = false;
+                continue;
+            }
+
+            System.out.println("____________________________________________________________");
+            System.out.println(userInput);
+            System.out.println("____________________________________________________________");
+        }
     }
 }
